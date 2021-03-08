f97d7bee4928d962b23d1011dfadfbd1a6e64d8a
igorbara <barabolia.gm@gmail.com>
2021-03-08 17:19:31 +0200

    lessont 15

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -1,22 +1,18 @@
 <?php
 
-namespace App\Http\Controllers\Blog;
+namespace App\Http\Controllers\Blog\Admin;
 
-use App\Models\BlogPost;
+use App\Models\BlogCategory;
 use Illuminate\Http\Request;
 
-class PostController extends BaseController
+class CategoryController extends BaseController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        $items = BlogPost::all();
+        $paginator = BlogCategory::paginate(5);
 
-        return view('blog.post.index', compact('items'));
+        return view('blog.admin.category.index', compact('paginator'));
     }
 
     /**
@@ -41,17 +37,6 @@ class PostController extends BaseController
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
@@ -70,17 +55,6 @@ class PostController extends BaseController
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
     {
         //
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', 'PostController')->names('blog.posts');
+});
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::group(['namespace' => 'Blog\Admin', 'prefix' => 'blog/admin'], function () {
+    Route::resource('categories', 'CategoryController')
+        ->only(['index', 'edit', 'update', 'create', 'store'])
+        ->name('*', 'categories');
 });
